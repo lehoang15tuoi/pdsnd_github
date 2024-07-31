50b6587b2072ee1c4364be5d18b27b113ab6e9ce
Udacity Cuong <lehoang15tuoi@gmail.com>
2024-07-31 17:57:40 -0400

    Add some comments

--- bikeshare.py
+++ bikeshare.py
@@ -6,6 +6,8 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+# functions below
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
